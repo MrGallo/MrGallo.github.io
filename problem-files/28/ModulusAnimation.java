52aae0ed818ee3b2ec7288d2c435bd8a2d912b23
MrGallo <daniel.gallo@ycdsbk12.ca>
2018-02-21 13:06:48 -0500

    Replace tabs with spaces

--- problem-files/28/ModulusAnimation.java
+++ problem-files/28/ModulusAnimation.java
@@ -1,10 +1,10 @@
 public class ModulusAnimation {
-	public static void main(String[] args) throws Exception {
-		for (int i = 0; i < 80; i++) {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 80; i++) {
             if (i % 10 == 0)
                 System.out.print(" Mr. Gallo is cool. \n");
 
             Thread.sleep(200);
         }
-	}
+    }
 }
