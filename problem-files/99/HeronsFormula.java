3169f8b916031fbb1c236974b01b78845bac1902
MrGallo <daniel.gallo@ycdsbk12.ca>
2018-02-28 14:58:19 -0500

    Add problem 99

--- problem-files/99/HeronsFormula.java
+++ problem-files/99/HeronsFormula.java
@@ -1,3 +1,9 @@
+/**
+ * The class HeronsFormula to calculate the area of a triangle given its three sides.
+ * <p>
+ *
+ * @author Graham Mitchell (modified my Mr. Gallo)
+ */
 public class HeronsFormula {
     public static void main(String[] args) {
         double a;
@@ -15,15 +21,22 @@ public class HeronsFormula {
         System.out.println("A triangle with sides 10,9,11 has an area of " + triangleArea(10, 9, 11));
         System.out.println("A triangle with sides 8,15,17 has an area of " + triangleArea(8, 15, 17));
     }
- 
+
+    /**
+     * Calculate the area of a triangle whose sides have lengths a, b, and c
+     *
+     * @param a
+     *          Side A
+     * @param b
+     *          Side B
+     * @param c
+     *          Side C
+     * @return the area
+     * @see <a href="http://www.mathwarehouse.com/geometry/triangles/area/herons-formula-triangle-area.php">Heron's Formula</a>
+     */
     public static double triangleArea(int a, int b, int c) {
-        // the code in this function computes the area of a triangle whose sides have lengths a, b, and c
-        double s, A;
+        double s = (a+b+c) / 2;  // semi-perimeter
 
-        s = (a+b+c) / 2;
-        A = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-
-        return A;
-        // ^ after computing the area, "return" it
+        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
     }
 }
