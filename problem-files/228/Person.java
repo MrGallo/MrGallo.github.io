95c037ccd51687314323db81f2094c02e7492229
mrgallo <daniel.gallo@ycdsb.ca>
2018-03-26 20:28:52 +0000

    Add problems 225-228

---
+++ problem-files/228/Person.java
@@ -0,0 +1,64 @@
+public class Person {
+
+    private String lastName;
+    private String firstName;
+    private int age;
+
+
+    /**
+     * Constructor for Person
+     * 
+     * @param firstName
+     *          String representing the person's first name.
+     * @param lastName
+     *          String representing the person's last name.
+     * @param age
+     *          Integer representing the person's age.
+     */
+    
+    
+    
+    
+
+    /**
+     * Getter for the person's name.
+     * @return
+     *          Full name in the format "first last".
+     */
+    
+    
+    
+    
+
+    /**
+     * Getter for age.
+     * @return
+     *          Integer representing the person's age.
+     */
+   
+   
+   
+   
+
+    /**
+     * Setter for name.
+     * @param firstName
+     *          Person's first name.
+     * @param lastName
+     *          Person's last name.
+     */
+    
+    
+    
+    
+
+    /**
+     * Setter for age.
+     * @param age
+     *          Person's age.
+     */
+   
+   
+   
+   
+}
