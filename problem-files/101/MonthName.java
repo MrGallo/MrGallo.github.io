9264b1e88bb2379aeec6f425ce07702882d2237b
mrgallo <daniel.gallo@ycdsb.ca>
2018-03-27 15:54:46 +0000

    add 101 and 100

---
+++ problem-files/101/MonthName.java
@@ -0,0 +1,25 @@
+public class MonthName {
+	public static String monthName(int month) {
+		String result;
+		// Your code goes in here.
+		
+		return result;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("Month 1: " + monthName(1));
+		System.out.println("Month 2: " + monthName(2));
+		System.out.println("Month 3: " + monthName(3));
+		System.out.println("Month 4: " + monthName(4));
+		System.out.println("Month 5: " + monthName(5));
+		System.out.println("Month 6: " + monthName(6));
+		System.out.println("Month 7: " + monthName(7));
+		System.out.println("Month 8: " + monthName(8));
+		System.out.println("Month 9: " + monthName(9));
+		System.out.println("Month 10: " + monthName(10));
+		System.out.println("Month 11: " + monthName(11));
+		System.out.println("Month 12: " + monthName(12));
+		System.out.println("Month 43: " + monthName(43));
+	}
+}
